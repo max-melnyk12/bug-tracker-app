ae16da7edef01b240825c1be0869189d8b4aba29
Max Melnyk <maksym.melnyk@lfg.com>
2022-08-06 19:22:14 -0500

    lots of changes

--- bug-tracker-app/src/app/projects/components/project-item/project-item.component.ts
+++ bug-tracker-app/src/app/projects/components/project-item/project-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-project-item',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectItemComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  project: Project = {} as Project
+
+  constructor(private router: Router) {
+    
+  }
 
   ngOnInit(): void {
+    console.log(this.project)
+  }
+
+  onEnterProject(){
+    
+    this.router.navigate(['bugs',this.project.id])
   }
 
 }
